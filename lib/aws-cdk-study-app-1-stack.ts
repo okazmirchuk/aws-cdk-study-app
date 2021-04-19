f39dd4f6f6fd848f621e1666644b9cdfac2e59a8
Oleksandr <kazmirchuk.oleksandr@gmail.com>
2021-04-19 07:24:42 +0300

    s3

--- lib/aws-cdk-study-app-1-stack.ts
+++ lib/aws-cdk-study-app-1-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import {Bucket, BucketEncryption} from "@aws-cdk/aws-s3";
 
 export class AwsCdkStudyApp1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'AwsCdkStudyAppBucket', {
+      encryption: BucketEncryption.S3_MANAGED,
+    })
+
+    new cdk.CfnOutput(this, 'AwsCdkStudyAppBucketExport', {
+      value: bucket.bucketName
+    })
   }
 }
