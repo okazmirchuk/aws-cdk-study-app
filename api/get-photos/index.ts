911ae77d3a2a795a6e5d74a7632eab1da35afa46
Oleksandr <kazmirchuk.oleksandr@gmail.com>
2021-04-29 11:52:16 +0300

    s3

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -1,11 +1,40 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResultV2, Context} from "aws-lambda";
+import {S3} from "aws-sdk";
 
-const bucketName = process.env.PHOTO_BUCKET_NAME;
+const bucketName = process.env.PHOTO_BUCKET_NAME!;
+
+const s3 = new S3();
+
+const generateUrl = async (object: S3.Object): Promise<{ filename: string, url: string}> => {
+    const url = await s3.getSignedUrlPromise('getObject', {
+        Bucket: bucketName,
+        Key: object.Key,
+        Expires: (24 * 60 * 60),
+    })
+
+    return {
+        filename: object.Key!,
+        url,
+    }
+}
 
 async function getPhotos(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResultV2> {
-    return {
-        statusCode: 200,
-        body: 'test'
+    try {
+        const {Contents: results} = await s3.listObjects({
+            Bucket: bucketName
+        }).promise()
+
+        const photos = await Promise.all(results!.map(result => generateUrl(result)))
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(photos)
+        }
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: e.message,
+        }
     }
 }
 
--- lib/aws-cdk-study-app-1-stack.ts
+++ lib/aws-cdk-study-app-1-stack.ts
@@ -3,36 +3,76 @@ import {Bucket, BucketEncryption} from "@aws-cdk/aws-s3";
 import {Runtime} from "@aws-cdk/aws-lambda"
 import {join} from 'path';
 import {NodejsFunction} from "@aws-cdk/aws-lambda-nodejs";
-import {getPhotos} from "../api/get-photos";
 import {BucketDeployment, Source} from "@aws-cdk/aws-s3-deployment";
+import {PolicyStatement} from "@aws-cdk/aws-iam";
+import {CorsHttpMethod, HttpApi, HttpMethod} from "@aws-cdk/aws-apigatewayv2";
+import {LambdaProxyIntegration} from "@aws-cdk/aws-apigatewayv2-integrations";
 
 export class AwsCdkStudyApp1Stack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const bucket = new Bucket(this, 'AwsCdkStudyAppBucket', {
-      encryption: BucketEncryption.S3_MANAGED,
-    })
+        const bucket = new Bucket(this, 'AwsCdkStudyAppBucket', {
+            encryption: BucketEncryption.S3_MANAGED,
+        })
 
-    new cdk.CfnOutput(this, 'AwsCdkStudyAppBucketExport', {
-      value: bucket.bucketName
-    })
+        new BucketDeployment(this, 'AwsCdkStudyAppPhotos', {
+            sources: [
+                Source.asset(join(__dirname, '..', 'photos'))
+            ],
+            destinationBucket: bucket
+        })
 
-    new BucketDeployment(this, 'AwsCdkStudyAppPhotos', {
-      sources: [
-        Source.asset(join(__dirname, '..', 'photos'))
-      ],
-      destinationBucket: bucket
-    })
+        const getPhotos = new NodejsFunction(this, 'AwsCdkStudyLambda', {
+            runtime: Runtime.NODEJS_12_X,
+            entry: join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
+            handler: 'getPhotos',
+            environment: {
+                PHOTO_BUCKET_NAME: bucket.bucketName
+            }
+        })
 
-    const getPhotos = new NodejsFunction(this, 'AwsCdkStudyLambda', {
-      runtime: Runtime.NODEJS_12_X,
-      entry: join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
-      handler: 'getPhotos',
-      environment: {
-        PHOTO_BUCKET_NAME: bucket.bucketName
-      }
-    })
+        const bucketContainerPermissions = new PolicyStatement();
 
-  }
+        bucketContainerPermissions.addResources(bucket.bucketArn)
+        bucketContainerPermissions.addActions('s3:ListBucket');
+
+        const bucketPermissions = new PolicyStatement()
+
+        bucketPermissions.addResources(`${bucket.bucketArn}/*`)
+        bucketPermissions.addActions('s3:GetObject', 's3:PutObject')
+
+        getPhotos.addToRolePolicy(bucketPermissions);
+        getPhotos.addToRolePolicy(bucketContainerPermissions);
+
+        const httpApi = new HttpApi(this, 'CdkStudyAppHttpApi', {
+            corsPreflight: {
+                allowOrigins: ['*'],
+                allowMethods: [CorsHttpMethod.GET],
+            },
+            apiName: 'photoApi',
+            createDefaultStage: true,
+        })
+
+        const lambdaIntegration = new LambdaProxyIntegration({
+            handler: getPhotos,
+        })
+
+        httpApi.addRoutes({
+            path: '/getAllPhotos',
+            methods: [
+                HttpMethod.GET,
+            ],
+            integration: lambdaIntegration
+        });
+
+        new cdk.CfnOutput(this, 'AwsCdkStudyAppBucketExport', {
+            value: bucket.bucketName
+        })
+
+        new cdk.CfnOutput(this, 'CdkStudyApi', {
+            value: httpApi.url!,
+            exportName: 'CdkStudyAppApiEndpoint',
+        })
+    }
 }
